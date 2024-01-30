440c7e60d6db82d03c91a7bd79bf475bc7c5a963
kimjames791 <kimjames791@gmail.com>
2024-01-30 14:26:37 -0800

    handle putchar

--- _putchar.c
+++ _putchar.c
@@ -1,13 +1,12 @@
-#include <main.h>
-
+#include <unistd.h>
 /**
  * _putchar - writes a char
  * @c: char to print
  * Return: 1 on success
- * -1 on error
+ * -1 on error and error is set appropriately
  */
 
-int _putcha(char c)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
