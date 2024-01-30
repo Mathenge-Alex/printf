c650e3d8e593a8c16747cc3b3fdefcfeecc63fa2
kimjames791 <kimjames791@gmail.com>
2024-01-30 15:50:51 -0800

    handle print rot

---
+++ print_rot.c
@@ -0,0 +1,33 @@
+# include "main.h"
+
+/**
+* print_rot13 - codes a text in rot1
+* @c: pointer to the rot
+*
+* Return: interger, strlen
+*/
+int print_rot13(char *c)
+{
+	int k;
+	int count = 0;
+	int z;
+	char p1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ";
+	char p2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm ";
+
+	if (c)
+
+	{
+		for (k = 0 ; *(c + k) != '\0' ; k++)
+		{
+			for (z = 0 ; s1[p] != '\0' ; z++)
+			{
+				if (*(c + k) == p1[z])
+				{
+					count += _putchar(p2[z])
+						break;
+				}
+			}
+		}
+	}
+	return (count);
+}
